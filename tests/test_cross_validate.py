ddb9c07501ca58918bb62aae8686f833e692319b
dantravers <djtravers1@sheffield.ac.uk>
2019-03-18 00:23:31 +0000

    fixed bug in feature removal and added tests for lagged features.

--- tests/test_cross_validate.py
+++ tests/test_cross_validate.py
@@ -14,21 +14,23 @@ import midas as md
 from pv_ml_models import cross_validate_grouped, coef_lr_grouped
 from model_definition import ModelDefinition
 
+# directory for all test data (hdfs & bench_*.csv)
+data_dir = 'C:/Users/Dan Travers/Documents/GitHub/ss-obs/tests'
+# setup model definitions: 
+kwargs_empty = {}
+lr = ModelDefinition('linear_r', ['month', 'hour'], 10, **kwargs_empty, text='Mth-Hr split')
+kwargs_grad = {"n_estimators": 100, "learning_rate" : 0.1, "max_depth" : 5, "random_state" : 0, "loss" : "ls"}
+grad = ModelDefinition('g_boost', [], 10, **kwargs_grad)
+ml_model_list = [lr, grad]
+# setup dates and weather / PV data
+start = datetime.date(2016,1,1)
+end = datetime.date(2018, 1, 1)
+weather = md.Midas(2)
+weather.config['hdf5']['store_name'] = os.path.join(data_dir, "midas_test_data.h5")
+power = pw.Power(2)
+power.config['hdf5']['store_name'] = os.path.join(data_dir, "power_test_data.h5")
+
 def test_cross_validate():
-    data_dir = 'C:/Users/Dan Travers/Documents/GitHub/ss-obs/tests'
-    # setup model definitions: 
-    kwargs_empty = {}
-    lr = ModelDefinition('linear_r', ['month', 'hour'], 10, **kwargs_empty, text='Mth-Hr split')
-    kwargs_grad = {"n_estimators": 100, "learning_rate" : 0.1, "max_depth" : 5, "random_state" : 0, "loss" : "ls"}
-    grad = ModelDefinition('g_boost', [], 10, **kwargs_grad)
-    ml_model_list = [lr, grad]
-    # setup dates and weather / PV data
-    start = datetime.date(2016,1,1)
-    end = datetime.date(2018, 1, 1)
-    weather = md.Midas(2)
-    weather.config['hdf5']['store_name'] = os.path.join(data_dir, "midas_test_data.h5")
-    power = pw.Power(2)
-    power.config['hdf5']['store_name'] = os.path.join(data_dir, "power_test_data.h5")
     tstats = pd.DataFrame([])
     # running cross validation through 3 models.
     for model in ml_model_list: 
@@ -56,3 +58,40 @@ def test_cross_validate():
     bench_stats = pd.read_csv(file_name, index_col=[0])
     print("Testing stats:")
     pd.testing.assert_frame_equal(tstats, bench_stats)
+
+def test_add_lagged_features():
+    lag1 = { 'lags' : {'irr' : [1, -1]}}
+    # running cross validation through 3 models.
+    run1= mr.ModelRun([4784], 
+                  [676], 
+                  power, weather,
+                  lr, 
+                  start, end, 
+                  forecast_hours_ahead=0, 
+                  lagged_variables=lag1, 
+                  sigma_clean=5, 
+                    verbose=2)
+    # test the results are equal by looking at feature df:
+    file_name = os.path.join(data_dir, "bench_lagged_features.csv" )
+    bench_results = pd.read_csv(file_name, index_col=0, parse_dates=True)
+    print("Testing lagged results:")
+    pd.testing.assert_frame_equal(run1.features, bench_results)
+
+def test_add_rolling_features():
+    rolling = { 'lags' : {'irr' : [-1, -2] }, 
+                'rolling' : { 'irr' : [3]} }
+    # running cross validation through 3 models.
+    run1= mr.ModelRun([4784], 
+                  [676], 
+                  power, weather,
+                  lr, 
+                  start, end, 
+                  forecast_hours_ahead=0, 
+                  lagged_variables=rolling, 
+                  sigma_clean=5, 
+                    verbose=2)
+    # test the results are equal by looking at feature df:
+    file_name = os.path.join(data_dir, "bench_rolling_features.csv" )
+    bench_results = pd.read_csv(file_name, index_col=0, parse_dates=True)
+    print("Testing rolling results:")
+    pd.testing.assert_frame_equal(run1.features, bench_results)
--- tests/test_load_obs_from_db.py
+++ tests/test_load_obs_from_db.py
@@ -4,9 +4,14 @@
 import numpy as np
 import pandas as pd
 import sys
+import os
+import shutil
 import datetime
 import midas
 import power as pw
+
+# setup common data
+current_dir = 'C:/Users/Dan Travers/Documents/GitHub/ss-obs/tests'
 
 def test_load_midas_obs_from_db():
     s = datetime.datetime(2017, 1, 1).date() 
@@ -33,3 +38,63 @@ def test_load_pvstream_obs_from_db():
 
     pd.testing.assert_frame_equal(power.metadata, power_metadata_bench)
     pd.testing.assert_frame_equal(power.obs, power_obs_bench)
+
+def test_load_additional_midas_obs_from_db():
+    s = datetime.datetime(2016, 12, 31).date() 
+    e = datetime.datetime(2017,1, 4).date()
+    orig = os.path.join(current_dir, 'midas_2sites_2days.h5')
+    hdf_scratch = os.path.join(current_dir, 'scratch.h5')
+    shutil.copy2(orig, hdf_scratch)
+    hdf_bench = os.path.join(current_dir, 'midas_3sites_4days.h5')
+    with pd.HDFStore(hdf_bench, 'r') as hdf:
+        weather_obs_bench = hdf.select('obs')
+    with pd.HDFStore(hdf_bench, 'r') as hdf:
+        weather_metadata_bench = hdf.select('metadata')
+
+    weather = midas.Midas(1)
+    weather.config['hdf5']['store_name'] = hdf_scratch
+    weather.load_data([842, 676, 384], s, e,  goto_db='')
+
+    #test results are as expected from initial load:
+    pd.testing.assert_frame_equal(weather.metadata.sort_index(), weather_metadata_bench.sort_index())
+    pd.testing.assert_frame_equal(weather.obs.sort_index(), weather_obs_bench.sort_index())
+
+    # save results to hdf, then create new object and lift results from hdf
+    weather.save_to_hdf()
+    weather2 = midas.Midas(1)
+    weather2.config['hdf5']['store_name'] = hdf_scratch
+    weather2.load_data([842, 676, 384], s, e,  goto_db='Never')
+    pd.testing.assert_frame_equal(weather2.metadata.sort_index(level=0), weather_metadata_bench.sort_index(level=0))
+    pd.testing.assert_frame_equal(weather2.obs.sort_index(level=0), weather_obs_bench.sort_index(level=0))
+
+    os.remove(hdf_scratch)
+
+def test_load_additional_pvstream_obs_from_db():
+    s = datetime.datetime(2016, 12, 31).date() 
+    e = datetime.datetime(2017,1, 4).date()
+    orig = os.path.join(current_dir, 'power_2sites_2days.h5')
+    hdf_scratch = os.path.join(current_dir, 'scratch.h5')
+    shutil.copy2(orig, hdf_scratch)
+    hdf_bench = os.path.join(current_dir, 'power_3sites_4days.h5')
+    with pd.HDFStore(hdf_bench, 'r') as hdf:
+        power_obs_bench = hdf.select('obs')
+    with pd.HDFStore(hdf_bench, 'r') as hdf:
+        power_metadata_bench = hdf.select('metadata')  
+
+    power = pw.Power(1)
+    power.config['hdf5']['store_name'] = hdf_scratch
+    power.load_data([3111, 6730, 8441], s, e,  goto_db='')
+
+    # test results are as expected from initial load:
+    pd.testing.assert_frame_equal(power.metadata.sort_index(level=0), power_metadata_bench.sort_index(level=0))
+    pd.testing.assert_frame_equal(power.obs.sort_index(), power_obs_bench.sort_index())
+
+    # save results to hdf, then create new object and lift results from hdf
+    power.save_to_hdf()
+    power2 = pw.Power(1)
+    power2.config['hdf5']['store_name'] = hdf_scratch
+    power2.load_data([3111, 6730, 8441], s, e,  goto_db='Never')
+    pd.testing.assert_frame_equal(power2.metadata.sort_index(level=0), power_metadata_bench.sort_index(level=0))
+    pd.testing.assert_frame_equal(power2.obs.sort_index(level=0), power_obs_bench.sort_index(level=0))
+
+    os.remove(hdf_scratch)
